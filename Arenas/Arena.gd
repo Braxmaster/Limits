7fd8f2ccd8b61cc20b24b32010ccaefe04dd00e1
branne93 <alebra@tudent.chalmers.se>
2018-11-09 17:32:14 +0100

    Arena now places characters on coded positions

--- Arenas/Arena.gd
+++ Arenas/Arena.gd
@@ -4,32 +4,43 @@ var battle_positions_left
 var battle_positions_right
 
 func _ready():
-	battle_positions_left = [Vector2(30, 30), Vector2(60, 60)]
-	battle_positions_left = [Vector2(170, 30), Vector2(140, 60)]
-	pass
+	#Put background in the background
+	#Testing
+	battle_positions_left = [Vector2(30, 140), Vector2(60, 170)]
+	battle_positions_right = [Vector2(170, 140), Vector2(140, 170)]
+	
+	var dummy_character_scene = load("res://Arenas/Dummy_Character.tscn")
+	var dummy_character1 = dummy_character_scene.instance()
+	var dummy_character2 = dummy_character_scene.instance()
+	place_battle_positions_left([dummy_character1, dummy_character2])
+	
+	var dummy_character3 = dummy_character_scene.instance()
+	var dummy_character4 = dummy_character_scene.instance()
+	place_battle_positions_right([dummy_character3, dummy_character4])
 	
 
 #Set background to sprite frames, background is animatedsprite since animated backgrounds would be cool
 func set_background(sprite_frames):
 	$Background.frames = sprite_frames
 	
-#At the moment there are no characters to work with, asume the character has  a Sprite node
+#At the moment there are no characters to work with, assume the character has  a Sprite node
 func place_battle_positions_left(characters_array):
-	number_of_characters = characters_array.size()
+	var number_of_characters = characters_array.size()
 	#Should raise an error but this will have to do
 	if(number_of_characters < battle_positions_left.size()):
 		print("Too many characters in this array for the arena")
 	
 	for i in range(0, number_of_characters):
-		characters_array[i].pos = battle_positions_left[i]
-		characters_array[i]._draw()
+		characters_array[i].position = battle_positions_left[i]
+		add_child(characters_array[i])
+		
 	
 func place_battle_positions_right(characters_array):
-	number_of_characters = characters_array.size()
+	var number_of_characters = characters_array.size()
 	#Should raise an error but this will have to do
-	if(number_of_characters < battle_positions_left.size()):
+	if(number_of_characters < battle_positions_right.size()):
 		print("Too many characters in this array for the arena")
 	
 	for i in range(0, number_of_characters):
-		characters_array[i].pos = battle_positions_right[i]
-		characters_array[i]._draw()
+		characters_array[i].position = battle_positions_right[i]
+		add_child(characters_array[i])
