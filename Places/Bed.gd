4558a074b514b7bb7d37faec975397dcfc180579
Jens H <jens.hulteberg@gmail.com>
2018-11-27 22:22:51 +0100

    Generalised dialogue a bit. Also fixed a new cool font.

--- Places/Bed.gd
+++ Places/Bed.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var voice = ["Oh hi there! Whats, up? I am a talking bed! Dont judge!", "I know what you do in the dead of night!"]
+
 func _ready():
 	pass
 
@@ -7,9 +9,7 @@ func _process(delta):
 	pass
 
 func do():
-	print("Bed is doing something!")
-	var next_level_resource = load("res://Tools/Dialogue.tscn")
-	var next_level = next_level_resource.instance()
-	get_node("../UI").add_child(next_level)
-	get_node("../UI/Dialogue/RichTextLabel").talk()
-#	get_node("../UI/Dialogue/RichTextLabel").talk()
+	var diag_system_res = load("res://Tools/Dialogue.tscn")
+	var diag_system = diag_system_res.instance()
+	get_node("../UI").add_child(diag_system)
+	get_node("../UI/Dialogue/Talker").talk(voice)
--- Tools/Talker.gd
+++ Tools/Talker.gd
@@ -1,34 +1,29 @@
 extends RichTextLabel
 
-var dialogue = ["Oh hi there!", "Whats, up?"]
+var dialogue
 var page = 0
 var wait_for_text = 0
 
 func _ready():
-	set_bbcode(dialogue[page])
-	set_visible_characters(0)
-	show()
+	pass
 
 func _process(delta):
-	print(page)
 	wait_for_text += 1
 	var INTERACT = Input.is_action_just_pressed("ui_accept")
 	if INTERACT and get_node("../../../Avatar").in_event and 30 < wait_for_text:
 		next_page()
 
 func _input(event):
-#	if event is InputEventKey and get_node("../../../Avatar").in_event:
-#		if event.pressed and event.scancode == 16777221 and not event.echo:
-#			print("Do stuff")
-#			next_page()
 	pass
 
 func _on_Timer_timeout():
 	visible_characters += 1
 
-func talk():
-	print("Start talking!")
-	print(dialogue[page])
+func talk(voice = ["I have nothing to say!"]):
+	dialogue = voice
+	set_bbcode(dialogue[page])
+	set_visible_characters(0)
+	show()
 	$Timer.start()
 
 
@@ -37,8 +32,6 @@ func next_page():
 	if page < dialogue.size():
 		set_bbcode(dialogue[page])
 		set_visible_characters(0)
-		print("Next page!")
-		print(dialogue[page])
 	else:
 		set_visible_characters(0)
 		page = 0
